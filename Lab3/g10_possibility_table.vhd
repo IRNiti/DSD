0af376d587b1d8ae9572d84081d3a184229c4a18
IRNiti <ioana.niti@mail.mcgill.ca>
2015-11-13 14:59:46 -0500

    Lab3 works properly -ish (some delays by half a clock cycle or 1 clock cycle)

--- Lab3/g10_possibility_table.vhd
+++ Lab3/g10_possibility_table.vhd
@@ -31,51 +31,55 @@ counter_table: process(CLK, TC_EN, TM_EN, TC_RST, TM_ADDR1, TM_ADDR2, TM_ADDR3, 
 
 begin	
 
+	
 	if (TC_RST = '1') then
 		TM_ADDR1 <= "000";
 		TM_ADDR2 <= "000";
 		TM_ADDR3 <= "000";
 		TM_ADDR4 <= "000";
-		TM_ADDR_Int <= (OTHERS =>'0');
+		last <= '0';
+--		TM_ADDR_Int <= (OTHERS =>'0');
 			
 	elsif ((TM_ADDR_Int(0) /= '0') and (TM_ADDR_Int(0) /= '1')) then
 		TM_ADDR1 <= "000";
 		TM_ADDR2 <= "000";
 		TM_ADDR3 <= "000";
 		TM_ADDR4 <= "000";
-		TM_ADDR_Int <= (OTHERS =>'0');
+--		TM_ADDR_Int <= (OTHERS =>'0');
+
 	
 	elsif rising_edge(clk) then
 		
-		if ((last /= '0') and (last /= '1')) then
-			last <= '0';
-		end if;
+		
 
 		if (TC_EN = '1') then
 			
-			if ( TC_RST = '0') then
+			if ( (TC_RST = '0') and (last /= '1')) then
 				TM_ADDR1 <= std_logic_vector((unsigned(TM_ADDR1) + "1")); -- TM_ADDR split into 4 parts  <--------
-				TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
+	--			TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
 				
 				if (TM_ADDR1 = "101") then
 					TM_ADDR1 <= "000";
 					TM_ADDR2 <= std_logic_vector((unsigned(TM_ADDR2) + "1")); -- <------
-					TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
+	--				TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
 			
 				if ((TM_ADDR2 = "101") and (TM_ADDR1 = "101")) then
 					TM_ADDR2 <= "000";
 					TM_ADDR3 <= std_logic_vector((unsigned(TM_ADDR3) + "1"));
-					TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
+		--			TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
 				
 					if ((TM_ADDR3 = "101") and (TM_ADDR2 = "101") and (TM_ADDR1 = "101")) then
 						TM_ADDR3 <= "000";
 						TM_ADDR4 <= std_logic_vector((unsigned(TM_ADDR4) + "1"));
-						TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
+		--				TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
 				
 						if (unsigned(TM_ADDR_Int) = "101101101101") then 
 							last <= '1';
-							TM_ADDR4 <= "000";
-							TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
+							TM_ADDR1 <= "101";
+							TM_ADDR2 <= "101";
+							TM_ADDR3 <= "101";
+							TM_ADDR4 <= "101";
+			--				TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
 							
 						--TM_ADDR_Int <= "000000000000";	
 					--	else TC_LAST <= '0';
@@ -87,10 +91,12 @@ begin
 			end if;
 		end if;
 	end if;
+	
 	TC_LAST <= last;
 	TM_ADDR_Int <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
 	Q_value <= TM_ADDR4 & TM_ADDR3 & TM_ADDR2 & TM_ADDR1;
 	TM_ADDR <= TM_ADDR_Int;	
+	
   end process;
   
  memory_table : process(CLK, TM_EN, TM_IN)
@@ -101,9 +107,12 @@ begin
 		if (TM_EN = '1') then 
 		
 			TM(to_integer(unsigned(Q_value))) <= TM_IN;
+			
+		else TM_OUT <= TM(to_integer(unsigned(Q_value)));
+		
 		end if;
-			TM_OUT <= TM(to_integer(unsigned(Q_value)));
-		end if;
+			
+	end if;
 	
 end process;
 			
