f72681fcf9c96a3f81f89e332a0ed117e77dae56
Blandry1 <benlandry5@gmail.com>
2015-12-03 13:58:51 -0500

    mastermind working, with button and switches implemented, and mapping

--- Lab5/MASTERMIND.vhd
+++ Lab5/MASTERMIND.vhd
@@ -8,21 +8,22 @@ port (clk : in std_logic;
 		P_generated : in std_logic; --buttons
 		Start : in std_logic;
 		Ready : in std_logic;
-		LED_countup : in std_logic;
+		switch_LED : in std_logic;
 				
 		mode : in std_logic;									  --switches
-		switch_REG : in std_logic_vector (1 downto 0);
 		sw : in std_logic_vector (3 downto 0); --for 15 downto 0, bits  = code, in 7-seg
+		modify_G : in std_logic;
 		
-		LED1 : out std_logic_vector(6 downto 0);  --displaying_LED's
-		LED2 : out std_logic_vector(6 downto 0); --work on
-		LED3 : out std_logic_vector(6 downto 0);
-		LED4 : out std_logic_vector(6 downto 0));
+		segment1 : out std_logic_vector(6 downto 0);  --displaying_LED's
+		segment2 : out std_logic_vector(6 downto 0); --work on
+		segment3 : out std_logic_vector(6 downto 0);
+		segment4 : out std_logic_vector(6 downto 0));
 end MASTERMIND;
 
 architecture behavior of MASTERMIND is 
 signal addr, G, gg, addrn,pattern : std_logic_vector(11 downto 0); --vectors
-signal cmp, srs, ps, grs, grl, srld, rpld : std_logic;    -- controller 
+signal cmp, srs, ps, grl, srld, rpld : std_logic;    -- controller 
+signal grs : std_logic_vector (1 downto 0);
 signal tmi,tme,tce,tcr,sol,tmout, last : std_logic; --possibility table
 signal tmi2,tme2,tce2,tcr2,sol2,tmout2, last2 : std_logic; --counter table
 signal P_generatedN : std_logic;
@@ -33,7 +34,11 @@ signal current_LED, next_LED : std_logic_vector (1 downto 0);
 signal display_scoreN, display_guessN, current_guess, view_REG : std_logic_vector (11 downto 0);
 signal ripple_out : std_logic;
 signal LED_value_int1,LED_value_int2,LED_value_int3,LED_value_int4 : std_logic_vector( 3 downto 0);
-signal GR1N, GR2N, GR3N, GR4N : std_logic_vector (2 downto 0);
+signal sw1ld, sw2ld, sw3ld, sw4ld : std_logic;
+signal grld1, grld2, grld3, grld4 : std_logic;
+signal led1r, led2r, led3r, led4r : std_logic;
+signal switchreg : std_logic;
+signal swI : std_logic_vector(2 downto 0);
 
 type state_type is (A,B,C,D,E,F,H,I,J);
 signal y_present : state_type;
@@ -47,14 +52,17 @@ port (G : in std_logic_vector(11 downto 0);
 		CLK : in std_logic;
 		clr : in std_logic;
 		
-		display_score : out std_logic_vector (11 downto 0);
-		display_guess : out std_logic_vector (11 downto 0);
+		sw: in std_logic_vector (2 downto 0);
+		sw1_LD, sw2_LD, sw3_LD, sw4_LD : in std_logic;
+		GR_LD1, GR_LD2, GR_LD3,GR_LD4 : in std_logic;
+		switch_REG : in std_logic;
+		LED1_R, LED2_R, LED3_R, LED4_R : in std_logic;
 		
 		--RP_LD : in std_logic;
 		SR_SEL : in std_logic;
 		P_SEL : in std_logic;
-		GR_SEL : in std_logic;
-		GR_LD : in std_logic;
+		GR_SEL : in std_logic_vector (1 downto 0);
+		--GR_LD : in std_logic;
 		SR_LD : in std_logic);
 		
 end component;
@@ -68,9 +76,15 @@ port (SC_CMP : in std_logic;
 		
 		SR_SEL : out std_logic;
 		P_SEL : out std_logic;
-		GR_SEL : out std_logic;
+		GR_SEL : out std_logic_vector (1 downto 0);
 		GR_LD : out std_logic;
 		SR_LD : out std_logic;
+		
+		sw1_LD, sw2_LD, sw3_LD, sw4_LD : out std_logic;
+		GR_LD1, GR_LD2, GR_LD3,GR_LD4 : out std_logic;
+		switch_REG : out std_logic;
+		LED1_R, LED2_R, LED3_R, LED4_R : out std_logic;
+		
 		RP_LD : out std_logic;
 		
 		TM_IN : out std_logic;
@@ -110,18 +124,18 @@ end component;
 
 begin
 
-g1 <= "000";
-g2 <= "000";
-g3 <= "000";
-g4 <= "000";
-gg <= g4 & g3 & g2 & g1;
+--g1 <= "000";
+--g2 <= "000";
+--g3 <= "000";
+--g4 <= "000";
+--gg <= g4 & g3 & g2 & g1;
 
-
-with switch_REG select --switch_REG
-	view_REG <= current_guess when "01", -- use -<- when guessing
-					display_guessN when "10",
-		         display_scoreN when "11",
-		         "000000000000" when others; 
+--
+--with switch_REG select --switch_REG
+--	view_REG <= current_guess when "01", -- use -<- when guessing
+--					display_guessN when "10",
+--		         display_scoreN when "11",
+--		         "000000000000" when others; 
 
 					
 --LED_value_int1 <= LED_value;
@@ -132,29 +146,28 @@ with switch_REG select --switch_REG
 
 --mapping between controller, datapath and possiblity table
 gate1: g10_mastermind_controller port map (SC_CMP => cmp, TC_LAST => last, START => Start, READY => Ready, CLK => clk, 
-														SR_SEL => srs, P_SEL => ps, GR_SEL => grs, GR_LD => grl, SR_LD => srld, RP_LD => rpld,
-														TM_IN => tmi, TM_EN => tme, TC_EN => tce, TC_RST => tcr, SOLVED => sol);
+													SR_SEL => srs, P_SEL => ps, GR_SEL => grs, GR_LD => grl, SR_LD => srld, RP_LD => rpld,
+													TM_IN => tmi, TM_EN => tme, TC_EN => tce, TC_RST => tcr, SOLVED => sol, 
+													sw1_LD => sw1ld, sw2_LD => sw2ld, sw3_LD => sw3ld, sw4_LD => sw4ld, GR_LD1 => grld1,
+													GR_LD2 => grld2, GR_LD3 => grld3, GR_LD4 => grld4, switch_REG => switchreg, 
+													LED1_R => led1r, LED2_R => led2r, LED3_R => led3r, LED4_R => led4r);
 														
 gate2: g10_mastermind_datapath port map (G => gg, EXT_PATTERN => pattern, TM_ADDR => addr, SC_CMP => cmp, CLK => clk, 
-								clr => Start, SR_SEL => srs, P_SEL => ps, GR_SEL => grs, GR_LD => grl, SR_LD => srld, 
-								display_guess => display_guessN, display_score => display_scoreN, GR1 => GR1N, GR2 => GR2N, 
-								GR3 => GR3N, GR4 => GR4N, sw => swI);
+								clr => Start, SR_SEL => srs, P_SEL => ps, GR_SEL => grs, SR_LD => srld, 
+								sw1_LD => sw1ld, sw2_LD => sw2ld, sw3_LD => sw3ld, sw4_LD => sw4ld, GR_LD1 => grld1, GR_LD2 => grld2,
+								GR_LD3 => grld3, GR_LD4 => grld4, switch_REG => switchreg, LED1_R => led1r, LED2_R => led2r, 
+								LED3_R => led3r, LED4_R => led4r, sw => swI );
+swI <= sw(3 downto 1);
 
 gate3: g10_possibility_table port map (TM_IN => tmi, TM_EN => tme, TC_EN => tce, TC_RST => tcr, CLK => clk, TC_LAST => last,
 													TM_ADDR => addr, TM_OUT => tmout);
 
 gate4: RandomPatternGenerator port map (P_generatedN => P_generated, clk => clk, EXT_PATTERN => pattern, Start => Start,
 													RP_LD => rpld);													
-gate5: g10_7_segment_decoder port map (code => LED_value_int1, RippleBlank_In => '0', RippleBlank_out => ripple_out, 
-													segments => LED1);
-gate6: g10_7_segment_decoder port map (code => LED_value_int2, RippleBlank_In => '0', RippleBlank_out => ripple_out, 
-													segments => LED2);		
-gate7: g10_7_segment_decoder port map (code => LED_value_int3, RippleBlank_In => '0', RippleBlank_out => ripple_out, 
-													segments => LED3);	
-gate8: g10_7_segment_decoder port map (code => LED_value_int4, RippleBlank_In => '0', RippleBlank_out => ripple_out, 
-													segments => LED4);												
+
+end behavior;									
 		
---swI <= sw(3 downto 1); --goes into each guess reg
+
 --		
 --mastermind : process (clk, Start, Ready, P_Generated, LED_countup, mode, switch_REG, LED_value)
 --LED_countup <= '0';
@@ -329,4 +342,3 @@ gate8: g10_7_segment_decoder port map (code => LED_value_int4, RippleBlank_In =>
 --end process;
 
 
-end behavior;
